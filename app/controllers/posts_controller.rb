51f6d7aeb4f773daea5c3360b2c055ec4fe303fb
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-06-03 20:58:09 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -36,6 +36,7 @@ class PostsController < ApplicationController
 
 
 private
+
   # Use callbacks to share common setup or constraints between actions.
   def set_post
     @post = Post.find(params[:id])
